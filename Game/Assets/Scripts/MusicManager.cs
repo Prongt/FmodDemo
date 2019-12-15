4efc9bb74900e00224d5b2cc7c1160bce4a59d76
Prongt <1413840+Prongt@users.noreply.github.com>
2019-12-15 20:57:35 +0000

    Implement music in unity

---
+++ Game/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+[RequireComponent(typeof(StudioEventEmitter))]
+public class MusicManager : MonoBehaviour
+{
+    [EventRef] public string fmodEvent;
+    StudioEventEmitter eventEmitter;
+    bool hatEquipped = false;
+
+
+    void Awake()
+    {
+        eventEmitter = eventEmitter ? eventEmitter : GetComponent<StudioEventEmitter>();
+
+        eventEmitter.Event = fmodEvent;
+    }
+
+    void Start()
+    {
+        eventEmitter.Play();
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        { 
+            hatEquipped = !hatEquipped;
+            //Debug.Log("Hat changed");
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("HatEquipped",  hatEquipped ? 1.0f : 0.0f);
+        }
+    }
+}
