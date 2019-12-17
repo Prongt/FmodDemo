1fa6219759707fb90b0e67835dfe92ea6e0587b5
Prongt <1413840+Prongt@users.noreply.github.com>
2019-12-17 16:49:24 +0000

    Added standard assets

--- Game/Assets/Scripts/MusicManager.cs
+++ Game/Assets/Scripts/MusicManager.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using FMOD.Studio;
-using FMODUnity;
+﻿using FMODUnity;
 using UnityEngine;
 
 [RequireComponent(typeof(StudioEventEmitter))]
 public class MusicManager : MonoBehaviour
 {
+    [ParamRef] public string actionParam;
+    public StudioEventEmitter eventEmitter;
     [EventRef] public string eventToPlay;
     [ParamRef] public string hatChangeParam;
-    [ParamRef] public string actionParam;
-    [ParamRef] public string distortParam;
-    
-    public StudioEventEmitter eventEmitter;
-    bool hatEquipped = false;
-    bool state = false;
-    bool distort = false;
-
+    bool hatEquipped;
+    bool state;
     
     void Awake()
     {
@@ -40,36 +34,21 @@ public class MusicManager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.H))
-        { 
-            hatEquipped = !hatEquipped;
-            //Debug.Log("Hat changed");
-            FMODUnity.RuntimeManager.StudioSystem.setParameterByName(hatChangeParam,  hatEquipped ? 1.0f : 0.0f);
-        }
+        if (Input.GetKeyDown(KeyCode.H)) EquipHat();
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ChangeState();
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            ChangeDistort();
-        }
+        if (Input.GetKeyDown(KeyCode.Space)) ChangeActionState();
     }
 
-    void ChangeState()
+    void EquipHat()
+    {
+        hatEquipped = !hatEquipped;
+        UiManager.DisplayHat = hatEquipped;
+        RuntimeManager.StudioSystem.setParameterByName(hatChangeParam, hatEquipped ? 1.0f : 0.0f);
+    }
+
+    void ChangeActionState()
     {
         state = !state;
-        //Debug.Log(state );
-        FMODUnity.RuntimeManager.StudioSystem.setParameterByName(actionParam, state ? 1.0f : 0.0f);
-    }
-    
-    void ChangeDistort()
-    {
-        distort = !distort;
-        //Debug.Log(distort);
-
-        FMODUnity.RuntimeManager.StudioSystem.setParameterByName(distortParam, distort ? 1.0f : 0.0f);
+        RuntimeManager.StudioSystem.setParameterByName(actionParam, state ? 1.0f : 0.0f);
     }
 }
---
+++ Game/Assets/Scripts/UiManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class UiManager : MonoBehaviour
+{
+    public static bool DisplayHat;
+    public GameObject hatIcon;
+
+    void Update()
+    {
+        hatIcon.SetActive(DisplayHat);
+    }
+}
