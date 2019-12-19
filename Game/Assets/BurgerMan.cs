ee4de523c7fc0a465e1403667bf91389e316d1c1
Prongt <1413840+Prongt@users.noreply.github.com>
2019-12-19 16:52:04 +0000

    Added robot voice lines

---
+++ Game/Assets/BurgerMan.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using FMODUnity;
+using UnityEngine;
+
+
+public class BurgerMan : MonoBehaviour
+{
+    public GameObject Burger;
+    public GameObject BurgerUI;
+    [EventRef] public string OnEnter;
+    [EventRef] public string OnRequestsBurger;
+    
+
+    bool playerInRange;
+    StudioEventEmitter OnEnterEmitter;
+    StudioEventEmitter OnBurgerEmitter;
+
+    void Awake()
+    {
+        // OnEnterEmitter = OnEnterEmitter ? OnEnterEmitter : GetComponent<StudioEventEmitter>();
+        // OnBurgerEmitter = OnBurgerEmitter ? OnBurgerEmitter : GetComponent<StudioEventEmitter>();
+
+        OnEnterEmitter = gameObject.AddComponent<StudioEventEmitter>();
+        OnBurgerEmitter = gameObject.AddComponent<StudioEventEmitter>();
+
+        OnEnterEmitter.Event = OnEnter;
+        OnBurgerEmitter.Event = OnRequestsBurger;
+    }
+
+    void Update()
+    {
+        if (!playerInRange) return;
+
+        if (!Input.GetKeyDown(KeyCode.E)) return;
+        
+        Burger.SetActive(true);
+            
+        StopEmitters();
+        OnBurgerEmitter.Play();
+    }
+
+    void StopEmitters()
+    {
+        OnBurgerEmitter.Stop();
+        OnEnterEmitter.Stop();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        BurgerUI.SetActive(true);
+        playerInRange = true;
+        
+        StopEmitters();
+        OnEnterEmitter.Play();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        BurgerUI.SetActive(false);
+        playerInRange = false;
+    }
+}
---
+++ Game/Assets/BurgerManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class BurgerManager : MonoBehaviour
+{
+    public GameObject Burger;
+    public GameObject BurgerUI;
+    [EventRef] public string OnEnter;
+    [EventRef] public string OnRequestsBurger;
+    
+
+    bool playerInRange;
+    StudioEventEmitter eventEmitter;
+
+    void Awake()
+    {
+        eventEmitter = eventEmitter ? eventEmitter : GetComponent<StudioEventEmitter>();
+    }
+}
